d9cca8351a9a0ca1d11c94942ebe66b4fe204d89
Mohaimin Al Aoun <mohaimin.al.aoun@vanderbilt.edu>
2020-02-23 17:39:06 -0500

    Updates

---
+++ precache-manifest.e6147c37fd120c6c257167d4ee8932de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b92a5d382aadecf46840ccccbf0279e",
+    "url": "/nafeez/index.html"
+  },
+  {
+    "revision": "c15452908cae161ef44b",
+    "url": "/nafeez/static/css/main.6bf0a109.chunk.css"
+  },
+  {
+    "revision": "ab22c56cecbccecebeec",
+    "url": "/nafeez/static/js/2.11c5c611.chunk.js"
+  },
+  {
+    "revision": "b330438950955e047355cec40f5a77fe",
+    "url": "/nafeez/static/js/2.11c5c611.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c15452908cae161ef44b",
+    "url": "/nafeez/static/js/main.410a586d.chunk.js"
+  },
+  {
+    "revision": "0e051132760fe6404cf5",
+    "url": "/nafeez/static/js/runtime-main.eb0ae124.js"
+  },
+  {
+    "revision": "36b5bab58a18b9c924861a4ccbf1a790",
+    "url": "/nafeez/static/media/Roboto-Bold.36b5bab5.ttf"
+  },
+  {
+    "revision": "082e978dfc9f1609e66b63f323ef4c6f",
+    "url": "/nafeez/static/media/menu.082e978d.svg"
+  }
+]);
